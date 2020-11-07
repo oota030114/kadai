fccafa9db36d0fd0184f8c21c9d9c2152182f2a1
hideyuki.ota <oota030114@gmail.com>
2020-11-07 19:25:53 +0900

    2020/11/07 19:25 change

--- study-01-search-main/03_search.py
+++ study-01-search-main/03_search.py
@@ -5,10 +5,8 @@ import os
 source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
 source_b=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
 
-
 ### csv読込み
-def readCSV():
-    csvFile =input("CSVファイルを指定してください（フルパス） >>> ")
+def readCSV(csvFile):
     if os.path.exists(csvFile):
         with open(csvFile) as f:
             listCSV = f.read()
@@ -16,7 +14,7 @@ def readCSV():
             return listCSV.split(',')
 
 ### リスト更新
-def updateSource(listCSV, source):    
+def updateSource(listCSV):    
     index = 0
     while index < len(listCSV):
         if not(listCSV[index] in source):
@@ -24,12 +22,27 @@ def updateSource(listCSV, source):
         index = index + 1
     return source
 
+### 検索ツール
+def search():
+    word =input("鬼滅の登場人物の名前を入力してください >>> ")
+    chk = word[-4:]
+    if (chk == ".csv") or (chk == ".CSV"):
+        if (os.path.isfile(word)):
+            ###csv読込み
+            listCSV = readCSV(word)
+            ret  = updateSource(listCSV)
+        else:
+            ret = "入力したCSVファイルは存在しません。"
+    else:
+        if not(word in source):
+            source.append(word)
+        ret = source
+    return ret
+
 ### メイン処理
 if __name__ == "__main__":
-    ###csv読込み
-    listCSV = readCSV()
-    ###リスト更新
-    updateSource(listCSV, source)
+    ### 検索ツール
+    source = search()
     ###更新結果
     print("鬼滅の登場人物の（更新前）⇒",source_b)
     print("鬼滅の登場人物の（更新後）⇒",source)
